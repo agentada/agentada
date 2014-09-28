e4c799b60fa325ca59b789a21d2ec40e355a5b6b
Daniel Jost <torpedojost@gmail.com>
2014-09-27 22:44:52 -0400

    Removed some debug logs.

--- api/services/UserInterests.js
+++ api/services/UserInterests.js
@@ -1,9 +1,8 @@
 module.exports = {
 
   generateInterestPercentages: function(userId, options, cb) {
-    User.findOne({ uid: userId }).exec(function(err, user) {
-      sails.log.debug(user);
-
+    User.findOne({ uid: userId }).exec(function(err, user) 
+    {
       if( options === null )
       {
         options = {
@@ -19,15 +18,13 @@ module.exports = {
         path : options.nextPage,
         method : 'GET' };
 
-      sails.log.debug( "WEEE PROCESSING " + options.totalLikes );
+      sails.log.debug( options.totalLikes + " Likes grabbed." );
 
       RESTService.sendGetRequest( fbOpts, function(err, likes) 
       {
-        sails.log.debug(likes);
-
         if( !likes )
         {
-          sails.log.debug( "what the shit " + options.totalLikes );
+          sails.log.debug( "Now we outta likes. Total: " + options.totalLikes );
           User.update(user.id, { interests: options.interestsAggregate, totalInterests: options.totalLikes }).exec(console.log);
           return false;
         }
@@ -35,7 +32,7 @@ module.exports = {
         {
           for( var i = 0; i < likes.data.length; i++ )
           {
-            sails.log.debug(likes.data[i].category);
+            //sails.log.debug(likes.data[i].category);
 
             if(options.interestsAggregate[likes.data[i].category] === undefined)
               options.interestsAggregate[likes.data[i].category] = 0;
